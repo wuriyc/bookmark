91b53303f9cfd61d370a27ff8daceffc36139bb9
wuriyc <kholder@kholder.co.kr>
2021-09-09 23:51:10 +0900

    4 bookmark

--- bookmark/views.py
+++ bookmark/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.views.generic.list import ListView
+from .models import Bookmark
+
+class BookmarkListView(ListView):
+    model = Bookmark
